9e6a706bc7ac673bc99a35e4fbdf189bcce20428
batomow <carlosa_miranda@outlook.com>
2017-11-27 15:40:26 -0600

    agregue la funcion split

--- main.cpp
+++ main.cpp
@@ -44,6 +44,21 @@ void cargarProceso(string linea)
 
 }
 
+vector<string> split(const string& s, const char& c)
+{
+	string buff{""};
+	vector<string> v;
+	
+	for(auto n:s)
+	{
+		if(n != c) buff+=n; else
+			if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+	}
+	if(buff != "") v.push_back(buff);
+	
+	return v;
+}
+
 
 int main()
 {
