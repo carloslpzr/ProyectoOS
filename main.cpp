41dab569a7b7b0b317d836f6343a12f516d2c3d6
carloslpzr <a01281326@itesm.mx>
2017-11-27 20:42:02 -0600

    fifo

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 vector<int> memPrincipal(2048);
 vector<int> swapping(4096);
 vector<int> marcos(128);
+queue<int> fifo;
 
 void accesar(string linea)
 {
@@ -20,7 +22,8 @@ void comenzar(string linea)
 
 void exit(string linea)
 {
-
+    cout << linea << endl;
+    cout << "Hasta luego!" << endl;
 }
 
 void fin(string linea)
