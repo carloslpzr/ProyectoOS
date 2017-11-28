72ec98e418256a11ff73edd900b0fef0c2a4d2a5
carloslpzr <a01281326@itesm.mx>
2017-11-27 20:25:06 -0600

    inicio

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int memPrincipal[2048];
+int swapping[4096];
+int marcos[128];
+
+void accesar(string linea)
+{
+
+}
+
+void comenzar(string linea)
+{
+
+}
+
+void exit(string linea)
+{
+
+}
+
+void fin(string linea)
+{
+
+}
+
+void liberar(string linea)
+{
+
+}
+
+void cargarProceso(string linea)
+{
+
+}
+
+
+int main()
+{
+    char comando;
+    string archivo, linea;
+    cin >> archivo;
+
+    fstream instrucciones;
+    instrucciones.open(archivo);
+    while(instrucciones.good())
+    {
+        getline (instrucciones, linea);
+        comando = linea[0];
+        switch(comando)
+        {
+            case 'A':
+                accesar(linea);
+                break;
+            case 'C':
+                comenzar(linea);
+                break;
+            case 'E':
+                exit(linea);
+                break;
+            case 'F':
+                fin(linea);
+                break;
+            case 'L':
+                liberar(linea);
+                break;
+            case 'P':
+                cargarProceso(linea);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    return 0;
+}
